d854ae0b383aaf379e5c3ed017d8e335a5bcabe5
james irick <james.e.irick@hotmail.com>
2014-07-18 20:36:47 -0400

    - fixed source formatting because of ide

--- js/view.js
+++ js/view.js
@@ -1,47 +1,48 @@
 var View = {
 
-	init: function()
-	{
-		this.reset();
-		/* register event listeners */
-		$("#grid td").click(ViewController.onGridClick)
-		$("#mode input[name=player]:radio").change(ViewController.onModeChange);
-		$("#sign input[name=character]:radio").change(ViewController.onCharacterChange);
-	},
+    init: function ()
+    {
+        this.reset();
+        /* register event listeners */
+        $("#grid td").click(ViewController.onGridClick)
+        $("#mode input[name=player]:radio").change(ViewController.onModeChange);
+        $("#sign input[name=character]:radio").change(ViewController.onCharacterChange);
+    },
 
-	updateUI: function(element, val)
-	{
-		document.getElementById(element).innerHTML = val;
-	},
+    updateUI: function (element, val)
+    {
+        document.getElementById(element).innerHTML = val;
+    },
 
-	alertUser: function(msg)
-	{
-		alert(msg);
-	},
+    alertUser: function (msg)
+    {
+        alert(msg);
+    },
 
-	reset: function()
-	{
-		$("#grid td").each(function(){
-			this.innerHTML = "";
-		});
-	},
+    reset: function ()
+    {
+        $("#grid td").each(function ()
+        {
+            this.innerHTML = "";
+        });
+    },
 
-	clearScore: function()
-	{
-		document.getElementById('x_score').innerHTML = 0;
-		document.getElementById('o_score').innerHTML = 0;
-	},
+    clearScore: function ()
+    {
+        document.getElementById('x_score').innerHTML = 0;
+        document.getElementById('o_score').innerHTML = 0;
+    },
 
-	showHide: function(element, isShowing)
-	{
-		var ele = document.getElementById(element);
-		if  (isShowing && ele)
-		{
-			ele.style.display = 'block';
-		}
-		else
-		{
-			ele.style.display = 'none';
-		}
-	}
+    showHide: function (element, isShowing)
+    {
+        var ele = document.getElementById(element);
+        if (isShowing && ele)
+        {
+            ele.style.display = 'block';
+        }
+        else
+        {
+            ele.style.display = 'none';
+        }
+    }
 };
